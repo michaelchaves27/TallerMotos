2097264299f83e9d7cb29c645c183af9fa01a117
AYM\mchaves <michavr27@gmail.com>
2024-06-12 20:15:48 -0600

    l4

--- TallerMotos/obj/Debug/net8.0/TallerMotos.Web.AssemblyInfo.cs
+++ TallerMotos/obj/Debug/net8.0/TallerMotos.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TallerMotos.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3d74be4c1904a1145d6a7f7f542c13de111b752")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb92e08f3f8772cd6953a37ab24c590e6f171bfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("TallerMotos.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TallerMotos.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
