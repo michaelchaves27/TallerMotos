c4c5d1889df1b31c987c50ccf2298d638e86a49b
AYM\mchaves <michavr27@gmail.com>
2024-06-12 20:08:00 -0600

    login

---
+++ TallerMotos/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TallerMotos.Web.Models;
+
+namespace TallerMotos.Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost("login")]
+        public IActionResult Login(LoginViewModel login)
+        {
+            if (login == null)
+            {
+                return View("Error");
+            }
+            if (login.User.Equals("admin") && login.Password.Equals("123456"))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Message = "Error en el acceso";
+                return View("index");
+            }
+
+        }
+    }
+}
---
+++ TallerMotos/Models/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TallerMotos.Web.Models
+{
+    public class LoginViewModel
+    {
+        public string User { get; set; }
+        public string Password { get; set; }
+    }
+}
