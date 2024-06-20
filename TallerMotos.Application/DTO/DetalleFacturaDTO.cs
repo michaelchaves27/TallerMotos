04875bf91e1c09b1cdebb6d59c33fa748b6666e9
Josue2505 <josuebf13@gmail.com>
2024-06-20 00:32:08 -0600

    ll

---
+++ TallerMotos.Application/DTO/DetalleFacturaDTO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TallerMotos.Infraestructure.Models;
+
+namespace TallerMotos.Application.DTO
+{
+    public record DetalleFacturaDTO
+    {
+        [Display(Name = "ID")]
+        public int ID { get; set; }
+        [Display(Name = "IDFactura")]
+        public int IDFactura { get; set; }
+
+        [Display(Name = "Codigo")]
+        public string Codigo { get; set; } = null!;
+
+        [Display(Name = "Nombre")]
+        public string Nombre { get; set; } = null!;
+
+        [Display(Name = "Cantidad")]
+        public string Cantidad { get; set; } = null!;
+
+        [Display(Name = "Precio")]
+        public string Precio { get; set; } = null!;
+
+        [Display(Name = "Impuesto")]
+        public string Impuesto { get; set; } = null!;
+
+        [Display(Name = "SubTotal")]
+        public string SubTotal { get; set; } = null!;
+
+        [Display(Name = "Total")]
+        public string Total { get; set; } = null!;
+
+        public virtual Facturas IdfacturaNavigation { get; set; } = null!;
+    }
+}
---
+++ TallerMotos/Controllers/DetalleFacturaController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TallerMotos.Application.Services.Interfaces;
+using TallerMotos.Web.Models;
+
+namespace TallerMotos.Web.Controllers
+{
+    public class DetalleFacturaController : Controller
+    {
+        private readonly IServiceDetalleFactura _serviceDetalleFactura;
+        public DetalleFacturaController(IServiceDetalleFactura serviceDetalleFactura)
+        {
+            _serviceDetalleFactura = serviceDetalleFactura;
+        }
+        public async Task<IActionResult> IndexDetalle(int id)
+        {
+            var collection = await _serviceDetalleFactura.ListAsync(id);
+            ViewData["Title"] = "IndexDetalle";
+            return View(collection);
+        }
+        public IActionResult ErrorHandler(string messageJson)
+        {
+            var errorMessages = JsonConvert.
+                DeserializeObject<ErrorMiddlewareViewModel>(messageJson);
+            ViewBag.ErrorMessages = errorMessages;
+            return View("ErrorHandler");
+        }
+    }
+}
